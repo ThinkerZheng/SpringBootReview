f0ae8d9fa95b27c11c13f62f76e3baa3edd1ae89
robin.zheng <robin.zheng@vechain.com>
2019-07-08 21:00:13 +0800

    add provider and consumer

---
+++ sbr-ServiceProvider1/src/main/java/org/sbr/ServiceProvider1/controller/TestController.java
@@ -0,0 +1,28 @@
+package org.sbr.ServiceProvider1.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.EurekaInstanceConfig;
+
+@RestController
+public class TestController {
+	protected Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@Autowired
+	private EurekaInstanceConfig eurekaInstanceConfig;
+	
+	@Value("${server.port}")
+	private int serverPort = 0;
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		this.logger.info(">>>>>>>>>>/hello, instanceId:{}, host:{}", eurekaInstanceConfig.getInstanceId(), eurekaInstanceConfig.getHostName(false));
+		return "Hello, Spring Cloud! My port is " + String.valueOf(serverPort);
+	}
+}
