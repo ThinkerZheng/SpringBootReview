7f2e2052c0c2677026560ffed4f55258237ac2eb
ThinkerZheng <guangbin@foxmail.com>
2019-07-24 00:49:02 +0800

    add final names to project

---
+++ UserProductComment/src/main/java/net/guangbin/UserProductCommentApplication.java
@@ -0,0 +1,13 @@
+package net.guangbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProductCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProductCommentApplication.class, args);
+	}
+
+}
